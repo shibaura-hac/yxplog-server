e865ccf76f2aaa77bae777f6e1f1794d47ed3525
Alkappa/alpaca-honke <akp@ymail.ne.jp>
2024-10-31 11:10:29 +0900

    add src/server.ts

---
+++ src/server.ts
@@ -0,0 +1,8 @@
+import { Application, Router, RouterContext } from "https://deno.land/x/oak@v6.5.0/mod.ts";
+
+const app = new Application();
+const router = new Router();
+
+app.addEventListener("listen", ({ hostname, port, secure }) => {
+
+});

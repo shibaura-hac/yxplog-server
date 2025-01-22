682a9416830cea23adf2d10953d5d39f39b61e1b
2gn <101851090+2gn@users.noreply.github.com>
2025-01-22 22:39:45 +0900

    added deno.json

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,8 @@
-import { Application, Router, RouterContext } from "https://deno.land/x/oak@v17.1.4/mod.ts";
+import {
+  Application,
+  Router,
+  RouterContext,
+} from "https://deno.land/x/oak@v17.1.4/mod.ts";
 
 const app = new Application();
 const router = new Router();
@@ -13,23 +17,26 @@ const router = new Router();
 //     console.log(e.error);
 // });
 
-router.get('/', (ctx: RouterContext) => {
-    ctx.response.body = "Hello, World!";
+router.get("/", (ctx: RouterContext) => {
+  ctx.response.body = "Hello, World!";
 });
 
-router.get('/get', (ctx: RouterContext) => {
-    ctx.response.body = "{}";
+router.get("/get", (ctx: RouterContext) => {
+  ctx.response.header = `{
+    "Content-Type": "application/json"
+  }`;
+  ctx.response.body = "{}";
 });
 
-router.get('/register', (ctx: RouterContext) => {
-    ctx.response.body = "{}";
+router.get("/register", (ctx: RouterContext) => {
+  ctx.response.body = "{}";
 });
 
-router.get('/edit', (ctx: RouterContext) => {
-    ctx.response.body = "{}";
+router.get("/edit", (ctx: RouterContext) => {
+  ctx.response.body = "{}";
 });
 
 app.use(router.routes());
 app.use(router.allowedMethods());
 
-app.listen({port: 8080});
+app.listen({ port: 8090 });
